580b0079b5b7afbb2cc8250b60471cfbef628eb2
Sougata <sougatadas9874@gmail.com>
2024-05-13 13:41:09 +0530

    fix: Meeting Room Ui.

--- components/MeetingRoom.tsx
+++ components/MeetingRoom.tsx
@@ -6,7 +6,6 @@ import {
   CallStatsButton,
   CallingState,
   PaginatedGridLayout,
-  SpeakerLayout,
   useCall,
   useCallStateHooks,
 } from "@stream-io/video-react-sdk";
@@ -14,23 +13,19 @@ import {
 import { useEffect, useState } from "react";
 import { redirect, useRouter, useSearchParams } from "next/navigation";
 
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from "./ui/dropdown-menu";
 import { Button } from "./ui/button";
 import EndCallButton from "./EndCallButton";
 
-import { Users, LayoutList, Check, Copy } from "lucide-react";
+import { Users, Check, Copy } from "lucide-react";
 
 import { cn } from "@/lib/utils";
+import { useMediaQuery } from "usehooks-ts";
 
 type CallLayoutType = "grid" | "speaker-left" | "speaker-right";
 
 const MeetingRoom = ({ id, user }: { id: string; user: any }) => {
+  const isMobile = useMediaQuery("(max-width: 600px)");
+
   const router = useRouter();
   const searchParams = useSearchParams();
   const isPersonalRoom = !!searchParams.get("personal");
@@ -55,16 +50,16 @@ const MeetingRoom = ({ id, user }: { id: string; user: any }) => {
     }, 2000);
   };
 
-  const CallLayout = () => {
-    switch (layout) {
-      case "grid":
-        return <PaginatedGridLayout />;
-      case "speaker-right":
-        return <SpeakerLayout participantsBarPosition="left" />;
-      default:
-        return <SpeakerLayout participantsBarPosition="right" />;
-    }
-  };
+  // const CallLayout = () => {
+  //   switch (layout) {
+  //     case "grid":
+  //       return <PaginatedGridLayout />;
+  //     case "speaker-right":
+  //       return <SpeakerLayout participantsBarPosition="left" />;
+  //     default:
+  //       return <SpeakerLayout participantsBarPosition="right" />;
+  //   }
+  // };
 
   const updateMemberList = async () => {
     await call?.updateCallMembers({ update_members: [{ user_id: user?.id }] });
@@ -83,7 +78,7 @@ const MeetingRoom = ({ id, user }: { id: string; user: any }) => {
     <section className="relative h-screen w-full overflow-hidden pt-4 text-white">
       <div className="relative flex size-full items-center justify-center">
         <div className=" flex size-full max-w-[1000px] items-center">
-          <CallLayout />
+          <PaginatedGridLayout />
         </div>
         <div
           className={cn("h-[calc(100vh-86px)] hidden ml-2", {
@@ -96,7 +91,7 @@ const MeetingRoom = ({ id, user }: { id: string; user: any }) => {
       <div className="flex-wrap fixed bottom-0 flex w-full items-center justify-center gap-5">
         <CallControls onLeave={() => router.push("/")} />
 
-        <DropdownMenu>
+        {/* <DropdownMenu>
           <div className="flex items-center">
             <DropdownMenuTrigger className="cursor-pointer rounded-2xl bg-[#19232d] px-4 py-2 hover:bg-[#4c535b]  ">
               <LayoutList size={20} className="text-white" />
@@ -116,28 +111,34 @@ const MeetingRoom = ({ id, user }: { id: string; user: any }) => {
               </div>
             ))}
           </DropdownMenuContent>
-        </DropdownMenu>
+        </DropdownMenu> */}
+
         <CallStatsButton />
         <button onClick={() => setShowParticipants((prev) => !prev)}>
-          <div className=" cursor-pointer rounded-2xl bg-[#19232d] px-4 py-2 hover:bg-[#4c535b]  ">
-            <Users size={20} className="text-white" />
-          </div>
-        </button>
-        <Button
-          className="rounded-2xl bg-[#19232d] px-4 py-2 hover:bg-[#4c535b] transition-all"
-          onClick={onCopy}
-        >
-          {copied ? (
-            <>
-              <Check className="mr-2 h-4 w-4" />
-              Copied
-            </>
-          ) : (
-            <>
-              <Copy className="mr-2 h-4 w-4" /> Copy Invite Link
-            </>
+          {!isMobile && (
+            <div className=" cursor-pointer rounded-2xl bg-[#19232d] px-4 py-2 hover:bg-[#4c535b]  ">
+              <Users size={20} className="text-white" />
+            </div>
           )}
-        </Button>
+        </button>
+        {!isMobile && (
+          <Button
+            className="rounded-2xl bg-[#19232d] px-4 py-2 hover:bg-[#4c535b] transition-all"
+            onClick={onCopy}
+          >
+            {copied ? (
+              <>
+                <Check className="mr-2 h-4 w-4" />
+                Copied
+              </>
+            ) : (
+              <>
+                <Copy className="mr-2 h-4 w-4" /> Copy Invite Link
+              </>
+            )}
+          </Button>
+        )}
+
         {!isPersonalRoom && <EndCallButton />}
       </div>
     </section>
